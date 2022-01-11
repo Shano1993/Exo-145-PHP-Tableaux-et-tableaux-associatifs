3ceb6d508d0956f01bf04566dd2b89302c8d267c
Shano1993 <stefan.hanotiau@gmail.com>
2022-01-11 12:27:35 +0100

    php

--- index.php
+++ index.php
@@ -2,32 +2,39 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
+$fruits = array("Apple", "Banana", "Orange");
+echo "Nombre d'élément dans le tableau" . " " . count($fruits) . "<br>";
 
 //Deuxieme ligne
 
+echo $fruits[1];
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
+$age = array("Peter" =>"35", "Ben" => "37", "Joe" => "43");
 
-echo "<br><br>Ben is " .  . " years old.<br><br>";
+echo "<br><br>Ben is " . $age["Ben"] . " years old.<br><br>";
 
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach ($age as $x => $y) {
+    echo "<br>Key=" . $x . ", Value=" . $y . "<br><br>";
 }
-
-
 
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-;
-
+sort($colors);
+echo print_r($colors) . "<br><br>";
 
 // Quatrieme ligne
 
+rsort($colors);
+echo print_r($colors) . "<br><br>";
+
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
+asort($age);
+foreach ($age as $key => $value) {
+    echo $value . "<br>";
+}
+
+
 
 
 
